8b4ca8862db18d9bbc08a96660c17fa1b6a70620
Jeetendra17 <123299954+Jeetendra17@users.noreply.github.com>
2024-09-06 11:58:45 +0530

    Update Program3.java

--- Program3.java
+++ Program3.java
@@ -1,11 +1,5 @@
-import java.util.Scanner;
-
 public class Program3 {
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        System.out.print("Enter your name: ");
-        String name = scanner.nextLine();
-        System.out.println("Hello, " + name + "!");
-        scanner.close();
+        System.out.println("Hello, World!");
     }
 }
